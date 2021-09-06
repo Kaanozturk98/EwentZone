d4e5e85b0274b814b562bc3cc054cd36683b876a
Kaanozturk98 <kaanozturk676@gmail.com>
2021-09-06 20:33:34 +0300

    complete header

--- frontend/src/layouts/home/HomeNavbar.tsx
+++ frontend/src/layouts/home/HomeNavbar.tsx
@@ -1,3 +1,51 @@
 export default function HomeNavbar() {
-  return <div>Home Navigation Bar</div>;
+  return (
+    <div className="bg-white">
+      <div className="container mx-auto h-24 flex items-center justify-between">
+        <a href="#">
+          <img
+            className="w-44 h-10"
+            src="https://classbento.co.uk/images/general/logo/wide-mulberry-classbento-logo-186.png"
+          />
+        </a>
+
+        <nav className="flex gap-x-8 text-lg font-bold">
+          <a
+            href="#"
+            className="text-opacity-90 text-gray-800 transition-all hover:text-red-900 hover:text-opacity-100"
+          >
+            Classes
+          </a>
+
+          <a
+            href="#"
+            className="text-opacity-90 text-gray-800 transition-all hover:text-red-900 hover:text-opacity-100"
+          >
+            Gifts
+          </a>
+
+          <a
+            href="#"
+            className="text-opacity-90 text-gray-800 transition-all hover:text-red-900 hover:text-opacity-100"
+          >
+            Teach
+          </a>
+
+          <a
+            href="#"
+            className="text-opacity-90 text-gray-800 transition-all hover:text-red-900 hover:text-opacity-100"
+          >
+            Contact
+          </a>
+
+          <a
+            href="#"
+            className="text-gray-800 text-opacity-90 transition-all hover:text-red-900 hover:text-opacity-100"
+          >
+            Login
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
 }
