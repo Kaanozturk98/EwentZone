c826a6757b983184608a90a4f6ae4172b3df35fb
Kaanozturk98 <kaanozturk676@gmail.com>
2021-11-29 01:09:30 +0300

    Update Ribbon.tsx

--- frontend/src/views/LandingPage/Ribbon.tsx
+++ frontend/src/views/LandingPage/Ribbon.tsx
@@ -4,7 +4,7 @@ import RibbonCategory from "components/RibbonCategory";
 export default function Ribbon() {
   return (
     <div className="bg-primary-100 h-20 mt-20 mb-64 md:my-24">
-      <div className="h-full lg:container px-4 xl:mx-auto grid grid-cols-2 md:grid-cols-6 gap-x-4 gap-y-4">
+      <div className="h-full lg:container lg:mx-auto px-4 grid grid-cols-2 md:grid-cols-6 gap-x-4 gap-y-4">
         <RibbonCategory
           label="Sanat"
           backgroundImage="https://classbento.co.uk/images/general/swirls/150h/03.png"
